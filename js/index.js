4ca95d3666483d0041e151b37408b5acc19123a9
mahanthathreyee <mahanthathreyee@gmail.com>
2019-04-04 13:38:23 +0530

    Switched into SocialBlade API

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 $(document).ready(()=>{
-    requestURL = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC-lHJZR3Gqxm24_Vd_AJ5Yw&key=" + youtubeAPI.key;
+    getSubCount();
+});
+
+function getSubCount(){
+    requestURL = "https://bastet.socialblade.com/youtube/lookup?query=UC-lHJZR3Gqxm24_Vd_AJ5Yw";
     $.ajax({
         method: "GET",
         url: requestURL
     }).done((value) => {
-        $('.subcountLeft').html(100000000 - value.items[0].statistics.subscriberCount);
+        $('.subcountLeft').html(100000000 - value);
     });
-});
+    setTimeout(getSubCount, 30000);
+}
