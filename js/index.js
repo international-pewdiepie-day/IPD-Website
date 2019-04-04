5b1981c1c6b67a39c1c05fb4c8097e024db1bde2
mahanthathreyee <mahanthathreyee@gmail.com>
2019-04-04 20:18:55 +0530

    Adde event countdown timer

--- js/index.js
+++ js/index.js
@@ -1,29 +1,38 @@
 'use strict';
 (function(){
-    const config = $.getJSON("./config.json", (json) => {
+    const config = $.getJSON("config.json", (json) => {
 
         console.log(json);
     });
 
     $(document).ready(() => {
         getSubCount();
-        // countdown();
+        countdown();
     });
     
     function getSubCount(){
-        requestURL = "https://bastet.socialblade.com/youtube/lookup?query=UC-lHJZR3Gqxm24_Vd_AJ5Yw";
+        let requestURL = "https://bastet.socialblade.com/youtube/lookup?query=UC-lHJZR3Gqxm24_Vd_AJ5Yw";
         $.ajax({
             method: "GET",
             url: requestURL
         }).done((value) => {
             $('.subcountLeft').html(100000000 - value);
         });
-        setTimeout(getSubCount, 30000);
+        setInterval(getSubCount, 7000);
     }
 
     function countdown(){
         // TO-DO: April 28 Countdown
         // #de003d
-        setTimeout(countdown, 1000);
+        
+        let eventDate = new Date("Apr 29, 2019");
+        let time_remaining = eventDate - new Date();
+
+        $('.days_left').html(Math.floor(time_remaining / (1000 * 60 * 60* 24)));
+        $('.hours_left').html(Math.floor((time_remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        $('.minutes_left').html(Math.floor((time_remaining % (1000 * 60 * 60)) / (1000 * 60)));
+        $('.seconds_left').html(Math.floor((time_remaining % (1000 * 60)) / 1000));
+        
+        setInterval(countdown, 1000);
     }
 })();
