492c0c75c4fb095f3814d30629ac9260c43c9cbf
rikuwu <41377725+rikupacito@users.noreply.github.com>
2019-04-04 19:58:12 +0800

    Inline CSS -miadevs
    Added inline CSS and some other things, courtesy of Mia

--- js/index.js
+++ js/index.js
@@ -1,14 +1,29 @@
-$(document).ready(()=>{
-    getSubCount();
-});
+'use strict';
+(function(){
+    const config = $.getJSON("./config.json", (json) => {
 
-function getSubCount(){
-    requestURL = "https://bastet.socialblade.com/youtube/lookup?query=UC-lHJZR3Gqxm24_Vd_AJ5Yw";
-    $.ajax({
-        method: "GET",
-        url: requestURL
-    }).done((value) => {
-        $('.subcountLeft').html(100000000 - value);
+        console.log(json);
     });
-    setTimeout(getSubCount, 30000);
-}
+
+    $(document).ready(() => {
+        getSubCount();
+        // countdown();
+    });
+    
+    function getSubCount(){
+        requestURL = "https://bastet.socialblade.com/youtube/lookup?query=UC-lHJZR3Gqxm24_Vd_AJ5Yw";
+        $.ajax({
+            method: "GET",
+            url: requestURL
+        }).done((value) => {
+            $('.subcountLeft').html(100000000 - value);
+        });
+        setTimeout(getSubCount, 30000);
+    }
+
+    function countdown(){
+        // TO-DO: April 28 Countdown
+        // #de003d
+        setTimeout(countdown, 1000);
+    }
+})();
